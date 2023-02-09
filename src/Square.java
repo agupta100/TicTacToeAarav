ce1307d06c91253c5914d18a27904f0d0a8b7059
agupta100 <aarav.gupta@menloschool.org>
2023-02-08 19:16:18 -0800

    Finished checking the win (program done)

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -18,7 +19,6 @@ public class Square {
     private int row;
     private int col;
     private boolean isWinningSquare;
-
     /**
      * Constructor to initialize one Square of the
      * TicTacToe board
@@ -61,9 +61,49 @@ public class Square {
         return this.marker;
     }
 
-    public void draw(Graphics g, int x, int y, int widthAndHeight, Image desired)
+    public void draw(Graphics g, TicTacToeViewer window)
     {
-        g.drawImage(desired, x, y, widthAndHeight, widthAndHeight, null);
+        int width = 800 / 5;
+        int height = 800 / 5;
+        Image xImage = new ImageIcon("Resources/X.png").getImage();
+        Image oImage = new ImageIcon("Resources/O.png").getImage();
+        Font font = new Font("Serif", Font.ITALIC, 45);
+        g.setFont(font);
+        g.setColor(Color.BLACK);
+        g.drawRect(window.WINDOW_WIDTH / 5 * (row + 1), window.WINDOW_HEIGHT / 5 * (col + 1), width, height);
+        if (marker.equals(TicTacToe.X_MARKER))
+        {
+            g.drawImage(xImage, width * (col + 1), height * (row + 1), 800 / 5,
+                    800 / 5, window);
+        }
+        else if (marker.equals(TicTacToe.O_MARKER))
+        {
+            g.drawImage(oImage, width * (col + 1), height * (row + 1), 800 / 5,
+                    800 / 5, window);
+        }
+        if (isWinningSquare)
+        {
+            g.setColor(Color.GREEN);
+            g.fillRect(width * (col + 1), height * (row + 1),800 / 5, 800 / 5);
+            if (marker.equals(TicTacToe.X_MARKER))
+            {
+                g.drawImage(xImage, width * (col + 1), height * (row + 1), 800 / 5,
+                        800 / 5, window);
+                g.setColor(Color.BLACK);
+                Font newFont = new Font("Serif", Font.PLAIN, 90);
+                g.drawString("X Wins", 300, 700);
+            }
+            else if (marker.equals(TicTacToe.O_MARKER))
+            {
+                g.drawImage(oImage, width * (col + 1), height * (row + 1), 800 / 5,
+                        800 / 5, window);
+                g.setColor(Color.BLACK);
+                Font newFont = new Font("Serif", Font.PLAIN, 90);
+                g.drawString("O Wins", 300, 700);
+            }
+        }
+
+
 
     }
 }
--- src/TicTacToeViewer.java
+++ src/TicTacToeViewer.java
@@ -4,11 +4,14 @@ import java.awt.image.ImageObserver;
 
 public class TicTacToeViewer extends JFrame {
 
-    private final int WINDOW_WIDTH = 800;
-    private final int WINDOW_HEIGHT = 800;
+    public final int WINDOW_WIDTH = 800;
+    public final int WINDOW_HEIGHT = 800;
     private final Image xImage;
     private final Image oImage;
     private TicTacToe t;
+    private Square square;
+
+
     public TicTacToeViewer(TicTacToe t)
     {
         this.t = t;
@@ -18,6 +21,8 @@ public class TicTacToeViewer extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.setVisible(true);
+        square = new Square(3, 3);
+
 
     }
 
@@ -34,12 +39,24 @@ public class TicTacToeViewer extends JFrame {
     public void paint(Graphics g)
     {
         setup(g);
+        Square[][] board = t.getBoard();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i][j].draw(g, this);
+            }
+        }
+        if (t.checkTie())
+        {
+            g.drawString("It's a Tie!", 300, 700);
+        }
     }
 
     public void setup(Graphics g)
     {
         g.setColor(Color.RED);
-        Font font = new Font("Times", Font.PLAIN, 45);
+        Font font = new Font("Serif", Font.ITALIC, 45);
         g.setFont(font);
 
         // Drawing numbers
@@ -49,17 +66,5 @@ public class TicTacToeViewer extends JFrame {
         g.drawString("0", WINDOW_WIDTH / 10, (WINDOW_HEIGHT  * 3) / 10);
         g.drawString("1", WINDOW_WIDTH / 10, WINDOW_HEIGHT / 2);
         g.drawString("2", WINDOW_WIDTH / 10, (WINDOW_HEIGHT * 7) / 10);
-        g.setColor(Color.BLACK);
-        g.drawLine(WINDOW_WIDTH / 5, WINDOW_HEIGHT / 5, WINDOW_WIDTH / 5, (WINDOW_HEIGHT * 4) / 5);
-        g.drawLine(WINDOW_WIDTH / 5, WINDOW_HEIGHT / 5, (WINDOW_WIDTH * 4) / 5, WINDOW_HEIGHT / 5);
-        g.drawLine(WINDOW_WIDTH / 5, (WINDOW_HEIGHT * 4) / 5, (WINDOW_WIDTH * 4) / 5, (WINDOW_HEIGHT * 4) / 5);
-        g.drawLine((WINDOW_WIDTH * 4) / 5, WINDOW_HEIGHT / 5, (WINDOW_WIDTH * 4) / 5, (WINDOW_HEIGHT * 4) / 5);
-        g.drawLine((WINDOW_WIDTH * 2) / 5, WINDOW_HEIGHT / 5, (WINDOW_WIDTH * 2) / 5, (WINDOW_HEIGHT * 4) / 5);
-        g.drawLine((WINDOW_WIDTH * 3) / 5, WINDOW_HEIGHT / 5, (WINDOW_WIDTH * 3) / 5, (WINDOW_HEIGHT * 4) / 5);
-        g.drawLine(WINDOW_WIDTH / 5, (WINDOW_HEIGHT * 2) / 5, (WINDOW_WIDTH * 4) / 5, (WINDOW_HEIGHT * 2) / 5);
-        g.drawLine(WINDOW_WIDTH / 5, (WINDOW_HEIGHT * 3) / 5, (WINDOW_WIDTH * 4) / 5, (WINDOW_HEIGHT * 3) / 5);
-
-//        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-
     }
 }
